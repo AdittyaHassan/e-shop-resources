55c3a683308905507cdd6a9cca2d7896e45e69c6
AdittyaHassan <nahidallhassan10@gmail.com>
2025-09-29 13:56:35 +0600

    init project setup

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+
+
+
+dotenv.config();
+const PORT = process.env.PORT || 6060;
+
+const app= express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
+
+app.listen(PORT, ()=>{
+    console.log(`server is running in ${PORT}`);
+    
+});
+
+
